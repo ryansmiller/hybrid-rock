90e469ac341aff0668786be0ebb47a79657a3368
Ryan Miller <ryansmiller@gmail.com>
2017-01-15 23:01:18 -0500

    Remove WP Header Image
    Remove Header Image from header.php. All images should be within the
    context of the content.

--- header.php
+++ header.php
@@ -37,16 +37,6 @@
 
 		<?php hybrid_get_menu( 'secondary' ); // Loads the menu/secondary.php template. ?>
 
-		<?php if ( get_header_image() && ! display_header_text() ) : // If there's a header image but no header text. ?>
-
-			<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home"><img class="header-image" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-
-		<?php elseif ( get_header_image() ) : // If there's a header image. ?>
-
-			<img class="header-image" src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="" />
-
-		<?php endif; // End check for header image. ?>
-
 		<div id="main" class="main">
 
 			<?php hybrid_get_menu( 'breadcrumbs' ); // Loads the menu/breadcrumbs.php template. ?>
