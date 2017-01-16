7090e25e4954f7a85bf5c9657bd7ac3857a4e533
Ryan Miller <ryansmiller@gmail.com>
2017-01-16 14:02:27 -0500

    Remove jQuery from CDN enqueue
    Default of to WordPress Core behavior and load locally

--- functions.php
+++ functions.php
@@ -97,18 +97,12 @@ add_action( 'wp_enqueue_scripts', 'hybrid_rock_scripts' );
  */
 function hybrid_rock_scripts() {
 
-
 	wp_deregister_style( 'font-awesome' );
 
 	wp_enqueue_style( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css', array(), '3.3.7', 'all' );
 
 	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0', 'all' );
 
-
-
-	wp_deregister_script( 'jquery' );
-
-	wp_enqueue_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), '3.1.1', false );
 
 	wp_enqueue_script( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js', array(), '3.3.7', true );
 
